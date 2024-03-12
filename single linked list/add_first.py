e6e576adf04adbfd0dcb8f66f1dfa7800caeaf57
umarulshahin <akkushahin666@gmail.com>
2024-03-12 11:43:47 +0530

    add begin

---
+++ single linked list/add_first.py
@@ -0,0 +1,35 @@
+#..................... new node at the biggning ............
+
+class Node:
+    
+    def __init__(self,data):
+        self.data=data
+        self.ref=None
+        
+class Link_list:
+    
+    def __init__(self):
+        
+        self.head=None
+    
+    def traverse(self):
+        
+        if not self.head :
+           print("linked List is Empty")
+        else:
+            n=self.head
+            while n:
+                print(n.data,"--->",end=" ")
+                n=n.ref
+
+    def add_begin(self,data):
+        new_node= Node(data)
+        new_node.ref=self.head
+        self.head=new_node
+        
+
+l=Link_list()
+l.add_begin(10)
+l.add_begin(20)
+
+l.traverse()
