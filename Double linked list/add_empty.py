d6350bd5a79ef935414baba1d84ecb18dd76e2fd
umarulshahin <akkushahin666@gmail.com>
2024-03-18 18:02:48 +0530

    add empty

---
+++ Double linked list/add_empty.py
@@ -0,0 +1,53 @@
+class Node:
+    
+    def __init__(self,data):
+        
+        self.data=data
+        self.nref=None
+        self.pref=None
+        
+class D_l_list:
+    
+    def __init__(self):
+        
+        self.head= None
+        
+    def f_print(self):
+        
+        if self.head is None:
+            print("linked list is None")
+            
+        else:
+            
+            n=self.head
+            while n:
+                print(n.data,"-->",end=" ")
+                n=n.nref
+                
+    def r_print(self):
+        print()
+        if self.head is None:
+            print("Linked list is None")
+            
+        else:
+            n=self.head
+            while n.nref:
+                n=n.nref
+                
+            while n:
+                print(n.data,"-->",end=" ")
+                n=n.pref
+                
+                
+    def add_empty(self,data):
+        if self.head is None:
+            new_node=Node(data)
+            self.head=new_node
+        else:
+            print("linked list not empty")
+
+
+d=D_l_list()
+d.add_empty(10)
+d.f_print()
+d.r_print()
